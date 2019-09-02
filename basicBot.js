66cb4a178a22aab5506bb7f9fc861331ae0572d6
Sonic73 <sonic_t_h@yahoo.com>
2019-09-02 02:35:54 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -905,7 +905,7 @@
 
             if (botCreatorIDs.indexOf(user.id) > -1) {
               console.log(true);
-                API.sendChat('@'+user.username+' :sparkles: :bow: :sparkles:');
+                API.sendChat('@'+user.username+' '+':sparkles: :bow: :sparkles:');
             } else if (basicBot.settings.welcome && greet) {
               console.log(false);
               console.log(botCreatorIDs);
